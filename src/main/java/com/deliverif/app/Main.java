ed9ea6a99f07248784927ab4a5d8eeec2b33f4cc
Natael Baffou <natael.baffou1@gmail.com>
2023-03-10 16:50:19 +0100

    Get boundaries

--- src/main/java/com/deliverif/app/Main.java
+++ src/main/java/com/deliverif/app/Main.java
@@ -7,6 +7,14 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            URL res = Main.class.getClassLoader().getResource("maps/smallMap.xml");
+            assert res != null;
+            CityMap cm = MapFactory.createMapFromPathFile(res.getPath());
+            System.out.println(cm);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
         JFrame main = new MainWindow();
     }
--- src/main/java/com/deliverif/app/models/map/CityMap.java
+++ src/main/java/com/deliverif/app/models/map/CityMap.java
@@ -8,15 +8,23 @@ import java.util.List;
 public class CityMap {
     private final Intersection warehouse;
     private final List<Street> streets;
+    private final Float minLatitude;
+    private final Float maxLatitude;
+    private final Float minLongitude;
+    private final Float maxLongitude;
 
 
-    private CityMap(Intersection warehouse, List<Street> streets) {
+    private CityMap(Intersection warehouse, List<Street> streets, Float minLatitude, Float maxLatitude, Float minLongitude, Float maxLongitude) {
         this.warehouse = warehouse;
         this.streets = streets;
+        this.minLatitude = minLatitude;
+        this.maxLatitude = maxLatitude;
+        this.minLongitude = minLongitude;
+        this.maxLongitude = maxLongitude;
     }
 
 
-    public static CityMap create(Intersection warehouse, List<Street> streets) {
-        return new CityMap(warehouse, streets);
+    public static CityMap create(Intersection warehouse, List<Street> streets, Float minLatitude, Float maxLatitude, Float minLongitude, Float maxLongitude) {
+        return new CityMap(warehouse, streets, minLatitude, maxLatitude, minLongitude, maxLongitude);
     }
 }
