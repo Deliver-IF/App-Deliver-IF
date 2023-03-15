e78c3c16aebdfbf357c22003fb0a3d0447578be6
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2023-03-15 17:29:48 +0100

    map generation using Line

--- src/main/java/com/deliverif/app/controllers/MainController.java
+++ src/main/java/com/deliverif/app/controllers/MainController.java
@@ -1,20 +1,20 @@
 package com.deliverif.app.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import lombok.Getter;
 
+@Getter
 public class MainController {
     @FXML
     private Label welcomeText;
     @FXML
-    private Canvas canvas;
+    private Pane mapPane;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
-    public Canvas getCanvas() {
-        return canvas;
-    }
+
 }
