ff68f8de09197c1f8dafa6e5648f1dc4d716f172
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2023-03-15 15:42:01 +0100

    test

--- src/main/java/com/deliverif/app/Main.java
+++ src/main/java/com/deliverif/app/Main.java
@@ -1,40 +1,23 @@
 package com.deliverif.app;
 
-import com.deliverif.app.models.map.CityMap;
-import com.deliverif.app.services.MapFactory;
-import com.deliverif.app.vues.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-/*        CityMap cm;
-        try {
-            URL res = Main.class.getClassLoader().getResource("maps/largeMap.xml");
-            assert res != null;
-            cm = MapFactory.createMapFromPathFile(res.getPath());
-            new MainWindow(cm);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
-
-        launch();
-
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-            stage.setTitle("Hello!");
-            stage.setScene(scene);
-            stage.show();
+    public static void main(String[] args) {
+        launch();
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.deliverif.app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires static lombok;
+    requires org.apache.commons.lang3;
+    requires java.xml;
+
+
+    opens com.deliverif.app to javafx.fxml;
+    exports com.deliverif.app;
+}
