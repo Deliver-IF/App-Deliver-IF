e89f8608e9fe426a7166c33d9991282641f7caad
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2023-03-15 15:13:29 +0100

    aled

--- src/main/java/com/deliverif/app/Main.java
+++ src/main/java/com/deliverif/app/Main.java
@@ -3,14 +3,19 @@ package com.deliverif.app;
 import com.deliverif.app.models.map.CityMap;
 import com.deliverif.app.services.MapFactory;
 import com.deliverif.app.vues.MainWindow;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        CityMap cm;
+/*        CityMap cm;
         try {
             URL res = Main.class.getClassLoader().getResource("maps/largeMap.xml");
             assert res != null;
@@ -18,8 +23,18 @@ public class Main {
             new MainWindow(cm);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }
+        }*/
 
+        launch();
 
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
     }
 }
