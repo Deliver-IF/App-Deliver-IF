226d2990d7f1a08185de5fad537d494c8ce5eaca
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2023-03-24 16:08:07 +0100

    Update Main.java

--- src/main/java/com/deliverif/app/Main.java
+++ src/main/java/com/deliverif/app/Main.java
@@ -1,39 +1,36 @@
 package com.deliverif.app;
 
-import com.deliverif.app.controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import java.awt.Dimension;
-
-
 public class Main extends Application {
+    private double screenWidth;
+    private double screenHeight;
+
     @Override
     public void init() throws Exception {
         super.init();
+
+        // Set main window size
+        screenWidth = Screen.getPrimary().getBounds().getWidth()*0.8;
+        screenHeight = Screen.getPrimary().getBounds().getHeight()*0.8;
     }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        // Set main window size
-        Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        double width = screenSize.getWidth()-screenSize.getWidth()*0.2;
-        double height = screenSize.getHeight()-screenSize.getHeight()*0.2;
-
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/main.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, width, height);
+        Scene scene = new Scene(root, screenWidth, screenHeight);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("DELIVR'IF");
         primaryStage.show();
-
-        MainController mainController = fxmlLoader.getController();
     }
 
     public static void main(String[] args) {
