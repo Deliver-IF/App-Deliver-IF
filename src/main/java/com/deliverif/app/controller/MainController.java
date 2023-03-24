229e766ccbb7c5fe93aeca860cfcda8221edc515
Benjamin CHAINTREUIL <benjamin@chaintreuil.net>
2023-03-24 15:24:05 +0100

    load map from file

--- src/main/java/com/deliverif/app/controller/MainController.java
+++ src/main/java/com/deliverif/app/controller/MainController.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 public class MainController {
     DataModel dataModel;
 
+
     // Map pane
     @FXML
     private Pane mapPane;
@@ -56,8 +57,6 @@ public class MainController {
     private Text deliveryDetailsText;
     @FXML Text itineraryText;
 
-    CityMap cityMap;
-
     ArrayList<Segment> courier1streets = new ArrayList<Segment>();
     ArrayList<Intersection> courier1deliverypoints = new ArrayList<Intersection>();
 
@@ -70,8 +69,6 @@ public class MainController {
     // Method called by the FXML loader after ressource file reading
     public void initialize() {
 
-        //        dummyFunction();
-//        System.out.println(courier1streets.toString());
     }
 
     @FXML
@@ -81,61 +78,10 @@ public class MainController {
         if (file != null) {
            try {
                 this.dataModel.loadMapFromFile(file);
-                System.out.println(dataModel);
             } catch (Exception exc) {
                 // handle exception...
             }
         }
-    }
-
-
-
-    public void dummyFunction() {
-        // TODO: Move to model, nothing showing because it's during init... so the windows isn't opened yet and the pane size = 0
-        try {
-            URL res = Main.class.getResource("maps/smallMap.xml");
-            assert res != null;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        Intersection intersection1 = new Intersection("25336175", (float) 4.863642, (float) 45.75301);
-        Intersection intersection2 = new Intersection("25336178", (float) 4.8658633, (float) 45.752552);
-        Intersection intersection3 = new Intersection("195270", (float) 4.865392, (float) 45.751556);
-        Intersection intersection4 = new Intersection("522100855", (float) 4.866085, (float) 45.751476);
-        Intersection intersection5 = new Intersection("1345284783", (float) 4.8660784, (float) 45.752106);
-
-        Segment segJeanRenoir = new Segment("Rue Jean Renoir",
-                (float) 179.80255,
-                intersection1,
-                intersection2
-        );
-        Segment segRueDeDauphine = new Segment("Rue du Dauphiné",
-                (float) 117.45386,
-                intersection2,
-                intersection3
-        );
-        Segment segRueRogerBrechan = new Segment("Rue Roger Brechan",
-                (float) 54.50388,
-                intersection3,
-                intersection4
-        );
-        Segment segClaudeRampon = new Segment("Imp. Claude Rampon",
-                (float) 70.188835,
-                intersection4,
-                intersection5
-        );
-
-        courier1streets.add(segJeanRenoir);
-        courier1streets.add(segRueDeDauphine);
-        courier1streets.add(segRueRogerBrechan);
-        courier1streets.add(segClaudeRampon);
-        courier1deliverypoints.add(intersection1);
-        courier1deliverypoints.add(intersection2);
-        courier1deliverypoints.add(intersection5);
-
-        map.drawBasemap(this.getMapPane(), cityMap);
-
-        map.displayCourierPath(this.getMapPane(), cityMap, courier1streets, courier1deliverypoints);
+        this.dataModel.getMapController().drawBasemap(this.mapPane, this.dataModel.getCityMap());
     }
 }
--- src/main/java/com/deliverif/app/model/DataModel.java
+++ src/main/java/com/deliverif/app/model/DataModel.java
@@ -1,6 +1,8 @@
 package com.deliverif.app.model;
 
+import com.deliverif.app.controller.MapController;
 import com.deliverif.app.services.MapFactory;
+import lombok.Getter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,13 +10,18 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
 public class DataModel {
+    @Getter
     CityMap cityMap;
+    @Getter
+    MapController mapController;
     public DataModel() {
         this.cityMap = null;
+        this.mapController = null;
     }
 
     public void loadMapFromFile(File mapFile) throws FileNotFoundException {
         this.cityMap = MapFactory.createMapFromPathFile(mapFile);
+        mapController = new MapController();
     }
 
 }
